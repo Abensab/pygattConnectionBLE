597b5fd763f924136a3b2d206944d82ec103da87
Abraham Benitez <abenitez@nayarsystems.com>
2018-09-18 13:09:07 +0200

    Building a good advanced menu and improve functions

--- main.py
+++ main.py
@@ -15,90 +15,32 @@ import uuid
 ADDRESS_TYPE = pygatt.BLEAddressType.public
 #ADDRESS_TYPE = pygatt.BLEAddressType.random
 
-#--Connection mode options
-CONN_AUTO   = 0
-CONN_MANUAL = 1
+def terminate(*args, **kwargs):
+    adapter.stop()
+    exit(1)
 
-#--Menu options
-SELECT_DEVICE      = 0
-SELECT_SERVICE     = 1
-SELECT_OPTION      = 2
-SELECT_CONN_MODE   = 3
+def dict_print(d, message):
+    opt =-1
+    print(20 * "-" + message + 20 * "-"+"\n")
+    while opt<0 or opt>=len(d):
+        print(''.join(["\t {0} - {1} \n".format(str(k), str(v[0])) for k, v in d.items()]))
+        print(20 * "-" + len(message)*"-" + 20 * "-"+"\n")
+        choice = raw_input("-SELECT OPTION: [0-" + str(len(d) - 1) + "]: ")
+        opt = check_option(choice, min = 0, max = len(d)-1)
+    return opt
 
-#--Methods in service
-CHAR_READ   = 0
-CHAR_WRITE  = 1
-CHAR_EXIT   = 2
+def conn_auto():
+    with open('config.json') as json_data_file:
+        data = json.load(json_data_file)
 
+        for k, v in data.items():
+            address = v["address"].decode("utf-8")
+            service_uuid = uuid.UUID(v["service_uuid"].decode("utf-8"))
+            connect(address, service_uuid=service_uuid)
 
-logging.basicConfig()
-logging.getLogger('pygatt').setLevel(logging.INFO)
-
-
-def check_input(input, min = 0, max = 100):
-    try:
-        return int(input) if min <= int(input) and int(input) <= max else -1
-    except:
-        print("ERROR you must introduce a valid option: 0-" + str(max) + "]")
-        return
-
-
-
-def print_menu(menu, devices = None):  ## Your menu design here
-    if menu == SELECT_CONN_MODE:
-        print("\n\n" + str(CONN_AUTO) + ". Automatic")
-        print(str(CONN_MANUAL) + ". Manual")
-        print(str(CONN_MANUAL + 1) + ". EXIT")
-        choice = raw_input("-SELECT CONNECTION MODE: [0-" + str(CONN_MANUAL + 1) + "]: ")
-        input = check_input(choice, min = 0,max = CONN_MANUAL+1)
-        return input
-
-    if menu == SELECT_DEVICE:
-        i = 0
-        print(29 * "-" + "DEVICES" + 29 * "-")
-        for device in devices:
-            print("\t-" + str(i) + " ADDRESS: " + device["address"] + (", NAME: "+device['name'] if device['name'] != None else ""))
-            i+=1
-        print("\t-" + str(i) + " EXIT")
-        print(67 * "-")
-        correct = False
-        while not correct:
-            choice = raw_input("-SELECT DEVICE: [0-" + str(i) + "]: ")
-            input = check_input(choice, min = 0, max = i)
-            if input == i:
-                adapter.stop()
-                exit(1)
-            elif input > -1:
-                correct = True
-                return devices[int(choice)]
-
-    elif menu==SELECT_SERVICE:
-        print(20 * "-" + "SERVICES AVAILABLES"+ 20 * "-")
-        i = 0
-        for uuid in devices.discover_characteristics().keys():
-            print '\t-' + str(i) + ' Service uuid: ' + str(uuid)
-            i += 1
-        print("\t-" + str(i) + " EXIT")
-        print(67 * "-")
-        correct = False
-        while not correct:
-            choice = raw_input("-SELECT SERVICE UUID: [0-" + str(i) + "]: ")
-            input = check_input(choice, min = 0, max = i)
-            
-            if input == i:
-                adapter.stop()
-                exit(1)
-            elif input > -1:
-                correct = True
-                return devices.discover_characteristics().keys()[input]
-
-    elif menu==SELECT_OPTION:
-        print(30 * "-" + "MENU" + 30 * "-")
-        print(str(CHAR_READ) + ". Read characteristic")
-        print(str(CHAR_WRITE) + ". Write characteristic")
-        print(str(CHAR_EXIT) + ". Disconnect and exit")
-        print(67 * "-")
-    
+def conn_Manual():
+    devices = adapter.scan(run_as_root = True, timeout = 3)
+    select_device_opt(devices)
 
 def read(device, uuid):
     try:
@@ -113,66 +55,95 @@ def read(device, uuid):
 def write(device, uuid):
     n = raw_input("Hoy many attributes you want to write?: ")
     message = []
-    for i in range(int(n)):
-        message.append(raw_input("Key: "))
-        message.append(raw_input("Value: "))
+
+    #for i in range(int(n)):
+    #    message.append(raw_input("Key: "))
+    #    message.append(int(raw_input("Value: ")))
+    message.append("OPT")
+    message.append(int(raw_input("Option: ")))
     pack = msgpack.packb(message, strict_types = True)
     device.char_write(uuid, bytearray(pack))
+
+
+#--Connection mode options
+CONN_OPT = dict({ 0:("AUTOMATIC CONNECTION", conn_auto), 1: ("MANUAL CONNECTION", conn_Manual), 2:("EXIT", terminate) })
+
+#--Menu options
+SELECT_DEVICE      = 0
+SELECT_SERVICE     = 1
+SELECT_OPTION      = 2
+SELECT_CONN_MODE   = 3
+
+#--Methods in service
+OPERATION_OPT = dict({ 0:("CHAR_READ", read), 1:("CHAR_WRITE", write), 2:("EXIT", terminate) })
+
+logging.basicConfig()
+#logging.getLogger('pygatt').setLevel(logging.INFO)
+logging.setLevel(logging.INFO)
+log = logging.getLogger(__name__)
+
+def check_option(opt, min = 0, max = 100):
+    try:
+        return int(opt) if min <= int(opt) and int(opt) <= max else -1
+    except:
+        print("ERROR you must introduce a valid option: 0-" + str(max) + "]")
+        return
+
+def select_device_opt(devices):
+    d = dict((i, ({"Device":devices[i]["address"], "Name":devices[i]["name"]}, connect)) for i in range(len(devices)))
+    d[len(d)]=("EXIT", None, terminate)
+    opt = dict_print(d, "SELECT DEVICE")
+    return d.get(opt)[1](d.get(opt)[0]["Device"])
+     
+def subscribe_service_opt(device, service_uuid=None):
+   
+    if service_uuid != None:
+        device.subscribe(service_uuid, callback = handle_data)
+        return service_uuid
+    else:
+        services = device.discover_characteristics().keys()
+        s = dict((i, (services[i], device.subscribe)) for i in range(len(services)))
+        s[len(s)]=("EXIT", terminate)
+        opt = dict_print(s, "SERVICE")
+        service_uuid = s.get(opt)[0]
+        s.get(opt)[1](service_uuid, callback = handle_data)
+        return service_uuid
+
 
 def handle_data(handle, value):
     """
     handle -- integer, characteristic read handle the data was received on
     value -- bytearray, the data returned in the notification
     """
-    print("---Received data: %s" % binascii.hexlify(value))
+    log.('Received notification on handle=0x%x, value=0x%s',
+                 handle, binascii.hexlify(value))
 
 def interactions(device=None, service_uuid=None):
     while True:
-        print_menu(SELECT_OPTION)
-        choice = raw_input("Enter your choice [0-" + str(CHAR_EXIT) + "]: ")
-        check_input(min, max, choice)
-        if int(choice) == CHAR_READ:
-            print("Read characteristic:\n\t")
-            read(device, service_uuid)
-        elif int(choice) == CHAR_WRITE:
-            print("Write characteristic")
-            write(device, service_uuid)
-        else:
-            device.disconnect()
-            return
+        opt = dict_print(OPERATION_OPT, "SERVICE OPTIONS")
+        print(OPERATION_OPT.get(opt)[1](device, service_uuid))
+
 def connect(address, service_uuid=None):
     try:
         print("Connecting to", address)
         device = adapter.connect(address, address_type = ADDRESS_TYPE)
-        if service_uuid==None:
-            service_uuid = print_menu(SELECT_SERVICE, device)
-        else:
-            device.subscribe(service_uuid, callback = handle_data)
-        print("Susccessful!")
+
+        while True:
+            service_uuid = subscribe_service_opt(device, service_uuid=service_uuid)     
+            if service_uuid!=None:
+                print("Susccessful!")
+                break
+            
         interactions(device=device, service_uuid=service_uuid)
     except pygatt.exceptions.NotConnectedError:
         print("Failed to connect to %s" % address)
 
 if __name__ == '__main__':
-  
     adapter = pygatt.GATTToolBackend(hci_device = "hci0")
     adapter.start()
-    option = print_menu(SELECT_CONN_MODE)
-    devices = adapter.scan(run_as_root = True, timeout = 3)
-    for d in devices:
-        print (d)
-    if  option == CONN_AUTO:
-        with open('config.json') as json_data_file:
-            data = json.load(json_data_file)
-        for k, v in data.items():
-            address = v["address"].decode("utf-8")
-            service_uuid = uuid.UUID(v["service_uuid"].decode("utf-8"))
-            connect(address, service_uuid=service_uuid)
-    else:
-        devices = adapter.scan(run_as_root = True, timeout = 3)
-        device = print_menu(SELECT_DEVICE, devices = devices)
-        address = device['address']
-        connect(address) 
-
-    adapter.stop()
-    sys.exit(1)
+    for k, v in CONN_OPT.items():
+        print("\t" + str(k) + " "+ v[0])
+    choice = raw_input("-SELECT CONNECTION MODE: [0-" + str(len(CONN_OPT) - 1) + "]: ")
+    opt = check_option(choice, min = 0, max = len(CONN_OPT) - 1)
+    CONN_OPT.get(opt)[1]()
+    terminate()
